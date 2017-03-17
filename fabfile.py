d7062b48cac9112a789fee1a73783288b2a2a46a
konstanting <constantine.gurnov@gmail.com>
2017-03-17 09:39:30 +0200

    fabric fix, run tests in virtual env

--- fabfile.py
+++ fabfile.py
@@ -5,5 +5,6 @@ def test():
 	""" Running all tests """
 	local('virtualenv -p /usr/local/lib/python2.7.9/bin/python .env')
 	local('./.env/bin/pip install -r requirements.txt')
-	local('./.env/bin/python thestackserver/manage.py test --noinput')
+	local('./runtests')
+	#local('./.env/bin/python thestackserver/manage.py test')
  
--- thestackserver/thestackserver/settings.py
+++ thestackserver/thestackserver/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
-    'django_nose',                                                              
+    'django_nose',
 ]
 
 MIDDLEWARE = [
